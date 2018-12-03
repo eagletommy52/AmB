6485bd73e1a1ed9294615c77a1ba3f4465fcd774
eagletommy52 <andrew.m.thompson@outlook.com>
2018-12-02 23:57:38 -0500

    Added burger menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './Nav.css';
 import ScrollableAnchor, { configureAnchors, goToTop } from 'react-scrollable-anchor';
+import { slide as Menu } from 'react-burger-menu'
+import './burger.css';
 configureAnchors({scrollDuration: 600})
 
 const Nav = (props) => {
   return (
     <ScrollableAnchor id={'home'}>
     <div className='navBar'>
-      <ul>
-        <li>
+      <div class="ambBurger">
+        
+        <Menu right>
+          <a href='#home' onClick={goToTop()}>Home</a>
+          <a href='#event'>Event</a>
+          <a href='#story'>Our Story</a>
+          <a href='#party'>Wedding Party</a>
+          <a href='#gallery'>Gallery</a>
+          <a href='#registry'>Registry</a>
+        </Menu>
+      </div>
+      <ul className="mainMenu">
+        <li id="leftBar">
           <ul className='sideNav'>
             <li><a href='#home' onClick={goToTop()}>Home</a></li>
             <li><a href='#event'>Event</a></li>
             <li><a href='#story'>Our Story</a></li>
           </ul>
         </li>
-        <li><img src={require('../../images/mbandrew.png')} alt="Mary Beth and Andrew"/></li>
-        <li>
+        <li id="amb"><img src={require('../../images/mbandrew.png')} alt="Mary Beth and Andrew"/></li>
+        <li id="rightBar">
           <ul className='sideNav'>
             <li><a href='#party'>Wedding Party</a></li>
             <li><a href='#gallery'>Gallery</a></li>
