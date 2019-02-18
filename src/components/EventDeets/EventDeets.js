b7dd1f6b0ab246979452d505f629e130bd6fa6b3
eagletommy52 <andrew.m.thompson@outlook.com>
2019-02-17 20:59:38 -0500

    added block problem info

--- src/components/EventDeets/EventDeets.js
+++ src/components/EventDeets/EventDeets.js
@@ -86,16 +86,17 @@ const EventDeets = (props) => {
             Accommodations
           </h3>
             <p>We have reserved a block under our names at the
-            <a href="https://www.thethayerhotel.com"> Thayer Hotel</a> where the reception will be held.
-            <br />
-            To reserve, call the Thayer at (845) 446-4731 
-            and ask reserve under the Daub/Thompson block.<br/>
-            Please book your room before April 2nd as the block<br/>
-            will be released to the public after that!
-            <br /><br/>
-            A second block of rooms is also reserved at the <br/><a href="https://www.ihg.com/holidayinnexpress/hotels/us/en/fort-montgomery/ftmny/hoteldetail?cm_mmc=GoogleMaps-_-EX-_-US-_-FTMNY">Holiday Inn Express</a> on Route 9W.  Just use group code DWT when you book for the group rate.
-              There are also a number of other hotels, bed & breakfasts and airbnb rentals available in the area
-            so we would recommend looking at some of the following locations:
+              <a href="https://www.thethayerhotel.com"> Thayer Hotel</a> where the reception will be held.
+              <br />
+              To reserve, call the Thayer at (845) 446-4731 
+              and ask reserve under the Daub/Thompson block.<br/>
+              Please book your room before April 2nd as the block<br/>
+              will be released to the public after that!<br/>
+              If you do have any issues booking in the block be<br/> sure to let Andrew or Mary Beth know and we'll get it fixed!<br/>
+              <br /><br/>
+              A second block of rooms is also reserved at the <br/><a href="https://www.ihg.com/holidayinnexpress/hotels/us/en/fort-montgomery/ftmny/hoteldetail?cm_mmc=GoogleMaps-_-EX-_-US-_-FTMNY">Holiday Inn Express</a> on Route 9W.  Just use group code DWT when you book for the group rate.
+                There are also a number of other hotels, bed & breakfasts and airbnb rentals available in the area
+              so we would recommend looking at some of the following locations:
             </p>
             <ul>
                 <li>
