9fa9e0fcfa1e7800dea7022b6d4dc171ae1de775
eagletommy52 <andrew.m.thompson@outlook.com>
2019-06-08 15:47:08 -0400

    Added Cali reception page

---
+++ src/components/California/California.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import '../RSVP/RSVP.css';
+
+const sendRSVPBack = invite => {
+  return new Promise((resolve, reject) => {
+    //`https://rsvp.somewhataccurate.com/rsvp/cali`
+    axios
+      .post(`https://rsvp.somewhataccurate.com/rsvp/cali`, invite, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then(result => {
+        result.data.success ? resolve({success:true}) : reject();
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+export class California extends Component {
+  state = {
+    name: '',
+    guests: 1,
+  }
+  handleInput = e => {
+    const nameIndex = e.target.name;
+    const nameValue = e.target.value;
+    this.setState(prevState => {
+      prevState[nameIndex] = nameValue;
+      return prevState;
+    });
+  }
+  handleRSVPSubmit = e => {
+    e.preventDefault();
+    sendRSVPBack(this.state).then(result => {
+      this.setState({response:'RSVP successfully sent!  We look forward to seeing you there'});
+    });
+  };
+  render() {
+    return (
+      <div
+        className='bgHero'
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          minHeight: '100vh',
+        }}>
+          <div className='rsvpContainer' style={{
+          textAlign: 'center'
+        }}>
+        <>
+          <h1>
+          California Reception
+          </h1>
+          In celebration of their wedding on June 1st, 2019, Mary Beth and Andrew are having a reception for all their West Coast friends and family in Claremont California on Saturday, July 27th 2019 at the Seaver House, 305 North College Avenue, Claremont, CA.  If you would like to attend, please kindly enter your name(s) and guest count below.<br/>
+          Name:<input name="guestName" id="guestNameInput" type="text" onChange={this.handleInput}/><br/>
+          Guests: <input type="number" defaultValue="1" onChange={this.handleInput} min="1" name="count" id="guestCountInput"/><br/>
+          
+          <button onClick={this.handleRSVPSubmit}>Submit</button><br/>
+          {this.state.response?this.state.response:null}
+        </>
+      </div>
+        </div>
+    )
+  }
+}
+
+export default California
