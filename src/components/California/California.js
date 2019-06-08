e7f724327ba184170b0f2a77ad164a215882b75b
eagletommy52 <andrew.m.thompson@outlook.com>
2019-06-08 16:15:38 -0400

    updated styles and locked out double submissions

--- src/components/California/California.js
+++ src/components/California/California.js
@@ -35,9 +35,15 @@ export class California extends Component {
   }
   handleRSVPSubmit = e => {
     e.preventDefault();
-    sendRSVPBack(this.state).then(result => {
-      this.setState({response:'RSVP successfully sent!  We look forward to seeing you there'});
-    });
+    if(this.state.guestName){
+      sendRSVPBack(this.state).then(result => {
+        this.setState({response:'RSVP successfully sent!  We look forward to seeing you there'});
+        document.getElementById('subButton').setAttribute('disabled', true)
+      });
+    } else {
+      this.setState({response:'Please enter a name to submit'})
+    }
+    
   };
   render() {
     return (
@@ -57,11 +63,14 @@ export class California extends Component {
           California Reception
           </h1>
           In celebration of their wedding on June 1st, 2019, Mary Beth and Andrew are having a reception for all their West Coast friends and family in Claremont California on Saturday, July 27th 2019 at the Seaver House, 305 North College Avenue, Claremont, CA.  If you would like to attend, please kindly enter your name(s) and guest count below.<br/>
-          Name:<input name="guestName" id="guestNameInput" type="text" onChange={this.handleInput}/><br/>
+          <form>
+          Name:<input required name="guestName" id="guestNameInput" type="text" onChange={this.handleInput}/><br/>
           Guests: <input type="number" defaultValue="1" onChange={this.handleInput} min="1" name="count" id="guestCountInput"/><br/>
           
-          <button onClick={this.handleRSVPSubmit}>Submit</button><br/>
+          <input type="submit" id="subButton" value="Submit" style={{cursor:'default'}} onClick={this.handleRSVPSubmit}/><br/>
+          </form>
           {this.state.response?this.state.response:null}
+
         </>
       </div>
         </div>
