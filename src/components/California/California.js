1825b59242c0ee7b7f1c27f8acd154a8af7d51d0
eagletommy52 <andrew.m.thompson@outlook.com>
2019-06-16 17:30:40 -0400

    added decline to cali rsvp

--- src/components/California/California.js
+++ src/components/California/California.js
@@ -39,7 +39,24 @@ export class California extends Component {
       sendRSVPBack(this.state).then(result => {
         this.setState({response:'RSVP successfully sent!  We look forward to seeing you there'});
         document.getElementById('subButton').setAttribute('disabled', true)
+        document.getElementById('decButton').setAttribute('disabled', true)
       });
+    } else {
+      this.setState({response:'Please enter a name to submit'})
+    }
+    
+  };
+  handleRSVPDecline = e => {
+    e.preventDefault();
+    if(this.state.guestName){
+      this.setState({count:0},()=>{
+        sendRSVPBack(this.state).then(result => {
+          this.setState({response:'Sorry you couldn\'t come--you\'ll be there in our thoughts!'});
+          document.getElementById('subButton').setAttribute('disabled', true)
+          document.getElementById('decButton').setAttribute('disabled', true)
+        });
+      })
+      
     } else {
       this.setState({response:'Please enter a name to submit'})
     }
@@ -62,12 +79,12 @@ export class California extends Component {
           <h1>
           California Reception
           </h1>
-          In celebration of their wedding on June 1st, 2019, Mary Beth and Andrew are having a reception for all their West Coast friends and family in at 2:00PM, Saturday, July 27th 2019 at the Seaver House, 305 North College Avenue, Claremont, CA.  Children of all ages are welcome, and dress code is dressy casual.  If you would like to attend, please kindly enter your name(s) and guest count below.<br/>
+          In celebration of their wedding on June 1st, 2019, Mary Beth and Andrew are having a reception for all their West Coast friends and family at 2:00PM, Saturday, July 27th 2019 at the Seaver House, 305 North College Avenue, Claremont, CA.  Children of all ages are welcome, and dress code is dressy casual.  If you would like to attend, please kindly enter your name(s) and guest count below, if not, please just let us know with your name and the deline button below!<br/>
           <form>
           Name:<input required name="guestName" id="guestNameInput" type="text" onChange={this.handleInput}/><br/>
-          Guests: <input type="number" defaultValue="1" onChange={this.handleInput} min="1" name="count" id="guestCountInput"/><br/>
+          Guests: <input type="number" defaultValue="1" onChange={this.handleInput} min="0" name="count" id="guestCountInput"/><br/>
           
-          <input type="submit" id="subButton" value="Submit" style={{cursor:'default'}} onClick={this.handleRSVPSubmit}/><br/>
+          <button id="subButton" style={{cursor:'default'}} onClick={this.handleRSVPSubmit}>Submit</button> <button id="decButton" style={{cursor:'default'}} onClick={this.handleRSVPDecline}>Decline</button> <br/>
           </form>
           {this.state.response?this.state.response:null}
 
